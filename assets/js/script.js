ccc898ffaf2b6ac6d03c5fbc3567e6c830ce5a37
Hillary Mason <hillary.mason@outlook.com>
2023-12-20 21:28:14 +0000

    added fetch code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,53 @@
+
+
+
 // Hello World!
-$(".search-button").on("click", function () {
-    console.log("search!");
-  });
+
   
+
+//text to speech API
+const speechURL = 'https://voicerss-text-to-speech.p.rapidapi.com/?key=%3CREQUIRED%3E&src=Hello%2C%20world!&hl=en-us&r=0&c=mp3&f=8khz_8bit_mono';
+const speechOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '6258e18b25mshd96a594dcf7fcb5p1b1fd6jsn955bae6d8f8b',
+		'X-RapidAPI-Host': 'voicerss-text-to-speech.p.rapidapi.com'
+	}
+};
+const wordsURL = 'https://wordsapiv1.p.rapidapi.com/words/incredible/definitions';
+const wordsOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '6258e18b25mshd96a594dcf7fcb5p1b1fd6jsn955bae6d8f8b',
+		'X-RapidAPI-Host': 'wordsapiv1.p.rapidapi.com'
+	}
+};
+
+//$(".search-button").on("click", function () {
+    
+  //});
+
+
+  function onsearch() {
+   
+         fetch(wordsURL, wordsOptions).then(function(response){
+            return response.json()
+         }) .then(function(data){
+            console.log("words data: ",data);
+         })
+
+    
+         fetch(speechURL, speechOptions).then(function(response){
+            console.log(response)           
+        })
+    
+  };
+
+  onsearch();
+
+
+
+//wordsapi
+
+
+
